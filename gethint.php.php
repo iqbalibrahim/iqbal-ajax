4f52ff5c86e1f9e7066dca9f8a89e05d6af075e6
iqbalibrahim <38452913+iqbalibrahim@users.noreply.github.com>
2018-04-24 15:14:35 +0700

    Add files via upload

---
+++ gethint.php.php
@@ -0,0 +1,31 @@
+<?php
+// array with names
+$a[] = "Annisa";
+$a[] = "Hendra Bayau";
+$a[] = "Reyhan Hime-Hime";
+$a[] = "Iqbal Ibrahim";
+$a[] = "Jihan Azza Syaqy";
+
+// get the q parameter from URL
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+//lookup all hints from array if $q is different from ""
+if ($q !== ""){
+	$q = strtolower($q);
+	$len = strlen($q);
+	foreach($a as $name){
+			if(stristr($q, substr($name, 0, $len))) {
+				if($hint === ""){
+					$hint = $name;
+				} else {
+					$hint .= ", $name";
+				}
+		}
+	}
+}
+
+//Output "no suggestion" if no hint was found o output correct values
+echo $hint == "" ? "Tidak Terdeteksi" : $hint;
+?>
